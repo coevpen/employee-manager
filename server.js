4fa3f6279cbdcf965864dad8b197833521edd4c2
coevpen <koinuevins@gmail.com>
2022-01-28 15:15:07 -0600

    remove routes, not needed

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const db_connection = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// app.use('/api', apiRoutes);
 
 app.use((req, res) => {
     res.status(4040).end();
