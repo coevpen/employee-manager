5587b80da5286a3c79cccc5e8234414d2575a03f
coevpen <koinuevins@gmail.com>
2022-01-29 15:39:16 -0600

    deleted index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// dependencies
-
-const deptQuery = require('./modules/Department');
-const roleQuery = require('./modules/Role');
-const employeeQuery = require('./modules/Employee');
-
-
-
-module.exports = {mainMenu};
