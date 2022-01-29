9deed62e737df992e2e96cce206372a81bfb6cbf
coevpen <koinuevins@gmail.com>
2022-01-29 15:39:00 -0600

    abandoned modules and placed everything in server.js

--- index.js
+++ index.js
@@ -1,73 +1,9 @@
 // dependencies
-const inquirer = require('inquirer');
+
 const deptQuery = require('./modules/Department');
 const roleQuery = require('./modules/Role');
 const employeeQuery = require('./modules/Employee');
 
 
-// will call the opening prompt that 
-function mainMenu(){
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            name: "choice",
-            choices: [
-                "View all departments",
-                "View all roles",
-                "View all employees",
-                "Add a department",
-                "Add a role",
-                "Add an employee",
-                "Update an employee role",
-                "Quit"
-            ]
-        }
-    ])
-    .then(chosen => {
-        switch(chosen.choice){
-            case 'View all departments':
-                deptQuery.showDepartments();
-                mainMenu();
-                break;
-
-            case 'View all roles':
-                roleQuery.showRoles();
-                mainMenu();
-                break;
-
-            case 'View all employees':
-                employeeQuery.showEmployees();
-                mainMenu();
-                break;
-
-            case 'Add a department':
-                deptQuery.addDepartment();
-                mainMenu();
-                break;
-
-            case 'Add a role':
-                roleQuery.addRole();
-                mainMenu();
-                break;
-
-            case 'Add an employee':
-                employeeQuery.addEmployee();
-                mainMenu();
-                break;
-
-            case 'Update an employee role':
-                employeeQuery.updateEmployeeRole();
-                mainMenu();
-                break;
-
-            case 'Quit':
-                process.exit();
-                
-            default:
-                break;
-        }
-    });
-};
 
 module.exports = {mainMenu};
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const startPrompt = require('./index');
-const mysql = require('mysql2/promise');
+//const startPrompt = require('./index');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const conTable = require('console.table');
 require('dotenv').config();
 
-// gets database connection and calls the start prompt
-async function main(){
-    const db_connection = await mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-    });
+// database connection configuration
+const db_connection = mysql.createConnection({
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+});
+
+// makes the database connection
+db_connection.connect(err => {
+    if(err) throw err;
+    console.log("connected");
+    main();
+})
+
+
+// prints the opening message once connection is made
+function main(){
 
     console.log(
         `         ---------------------------------------------------
@@ -21,7 +33,141 @@ async function main(){
          ---------------------------------------------------`
     );
 
-    startPrompt.mainMenu();
+    mainMenu();
 }
 
-main();
+// will call the opening prompt that 
+function mainMenu(){
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "choice",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Quit"
+            ]
+        }
+    ])
+    .then(chosen => {
+        switch(chosen.choice){
+            case 'View all departments':
+                showDepartments();
+                break;
+
+            case 'View all roles':
+                showRoles();
+                break;
+
+            case 'View all employees':
+                showEmployees();
+                break;
+
+            case 'Add a department':
+                addDepartment();
+                break;
+
+            case 'Add a role':
+                addRole();
+                break;
+
+            case 'Add an employee':
+                addEmployee();
+                break;
+
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+
+            case 'Quit':
+                db_connection.end();
+                process.exit();
+                
+            default:
+                break;
+        }
+    });
+};
+
+
+/*
+*
+*   EMPLOYEE FUNCTIONS
+*
+*/
+
+// show table for Employees: id, first and last name, job title, department, salary, manager reporting
+function showEmployees(){
+        let query = `SELECT employee.id,
+                        employee.first_name,
+                        employee.last_name,
+                        employee_role.title,
+                        departments.name AS department,
+                        employee_role.salary
+                        CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee
+                        LEFT JOIN employee_role ON employee.role_id = employee_role.id
+                        LEFT JOIN departments ON employee_role.department_id = departments.id
+                        LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+    db_connection.query(query, (err, res) => {
+        if(err) throw err;
+        console.log("\n");
+        console.table(res);
+        mainMenu();
+    });
+
+}
+
+// adds an employee
+function addEmployee(){
+    console.log("function called");
+
+}
+
+// updates employee role
+function updateEmployeeRole(){
+    console.log("function called");
+
+}
+
+
+
+/*
+*
+*   ROLE FUNCTIONS
+*
+*/
+
+// show table for roles: job title, role id, department it belongs to, salary for role
+function showRoles(){
+    console.log("function called");
+}
+
+// adds a role
+function addRole(){
+    console.log("function called");
+}
+
+
+
+/*
+*
+*   DEPARTMENTS FUNCTIONS
+*
+*/
+
+// show table for departments and department ids
+function showDepartments(){
+    console.log("function called");
+
+}
+
+function addDepartment(){
+    console.log("function called");
+
+}
